455fbeaa98bae2867a995aa5cf277d98b00ce689
Dhanushka <dhanushkamcr@gmail.com>
2025-11-14 14:41:35 -0600

    Configured firebase in project

---
+++ src/lib/firebaseConfig.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDfdO9uZNlyMfwMzRu-bN1OxVVf1uaFCwU",
+  authDomain: "react-ecommerce-app-310d3.firebaseapp.com",
+  projectId: "react-ecommerce-app-310d3",
+  storageBucket: "react-ecommerce-app-310d3.firebasestorage.app",
+  messagingSenderId: "803741296216",
+  appId: "1:803741296216:web:bf80cd00bd7b7363510c2d"
+};
+
+const app = initializeApp(firebaseConfig);
