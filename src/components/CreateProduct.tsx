48a76239b34b40bf284ef18819d100dfa7e08eaf
Dhanushka <dhanushkamcr@gmail.com>
2025-11-19 17:01:18 -0600

    Create form container and center it on viewport

--- src/components/CreateProduct.tsx
+++ src/components/CreateProduct.tsx
@@ -1,8 +1,13 @@
 
 const CreateProduct = () => {
-    
+
     return (
-        <h1>Add Product</h1>
+        <div className="d-flex justify-content-center align-items-center min-vh-100">
+            <div className="card border-0 shadow-lg p-5">
+                <h2 className="fw-bold text-center mb-2">Add New Product</h2>
+                <p className="text-center mb-4">Fill in the details to add a new product to your store</p>
+            </div>
+        </div>
     );
 };
 
