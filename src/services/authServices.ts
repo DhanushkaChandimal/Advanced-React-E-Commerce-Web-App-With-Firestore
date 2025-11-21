96932ae2e3941d14c315ac82deca776bcd9778bd
Dhanushka <dhanushkamcr@gmail.com>
2025-11-21 17:11:52 -0600

    Save max user and product id in firestore to make generate new Id s

--- src/services/authServices.ts
+++ src/services/authServices.ts
@@ -1,14 +1,34 @@
-import { addDoc, collection } from "firebase/firestore";
-import type { AuthUser } from "../types/types";
+import { addDoc, collection, doc, getDoc, updateDoc } from "firebase/firestore";
+import type { AuthUser, FirestoreConfig } from "../types/types";
 import { db } from "../lib/firebaseConfig";
 
 type CreateUserData = Omit<AuthUser, 'id'>;
 
 const USER_COLLECTION = 'users';
+const SETTINGS = 'settings';
+const CONFIG = 'config';
 
 export const authService = {
+    getConfigs: async (): Promise<FirestoreConfig> => {
+        const docSnapshot = await getDoc(doc(db, SETTINGS, CONFIG));
+        const configs = docSnapshot.data();
+        console.log(configs)
+        return configs as FirestoreConfig;
+    },
+
+    updateConfigs: async (configData: FirestoreConfig): Promise<boolean> => {
+        const docRef = doc(db, SETTINGS, CONFIG);
+        await updateDoc(docRef, {
+            maxProductId: configData.maxProductId,
+            maxUserId: configData.maxUserId
+        });
+        console.log("Hey UYpdated " + configData)
+        return true;
+    },
+
     createUser: async (userData: CreateUserData): Promise<AuthUser> => {
-        const newId = 1;
+        const firestoreConfig = await authService.getConfigs();
+        const newId = firestoreConfig.maxUserId + 1;
         
         const newUser: AuthUser = {
             id: newId,
@@ -16,6 +36,12 @@ export const authService = {
         };
 
         await addDoc(collection(db, USER_COLLECTION), newUser);
+
+        await authService.updateConfigs({
+            maxProductId: firestoreConfig.maxProductId,
+            maxUserId: newId,
+        });
+        console.log("nrewID  " + newId)
         
         return newUser;
     },
--- src/types/types.ts
+++ src/types/types.ts
@@ -16,3 +16,8 @@ export interface AuthUser {
    firstName: string;
    lastName: string;
 }
+
+export interface FirestoreConfig {
+   maxProductId: number;
+   maxUserId: number;
+}
